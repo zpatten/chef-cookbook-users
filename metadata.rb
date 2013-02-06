7d468cdbd30f3ecb423b727e6c694ece251ceda6
Zachary Patten <zpatten@jovelabs.com>
2013-02-05 23:54:02 -0800

    minor version bump

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "zpatten@jovelabs.com"
 license          "Apache 2.0"
 description      "Installs/Configures users"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.0"
+version          "1.3.0"
